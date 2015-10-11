2ba09463146df3a5f752c5659fa0afcd58a60719
Mathieu Kessler <mathieu.kessler@upct.es>
2015-10-11 20:16:14 +0200

    Rcode: generates nodes_figures, nodes_info & links.
    Generates nodes_info with acronyms for programmes (labelSpanish),
    links2.csv, and nodes_figures.csv (students & proportion of male
    students), where the data are based on SIIU.

--- R/1-generate-nodes-information.R
+++ R/1-generate-nodes-information.R
@@ -13,6 +13,12 @@ alumnos <- alumnos %>%
          nivel2 = paste(nivel1, CENTRO_ACRONIMO, sep = "/"),
          nivel3 = paste(nivel2, SUBTIPO_ESTUDIO_ID, sep = "/"),
          nivel4 = paste(nivel3, PLAN_ID, sep = "/"))
+## -----------------------------------------------------------------------------
+##
+## We create the file with nodes information (labels, color, etc..)
+##
+## -----------------------------------------------------------------------------
+
 nodes_info <- data.frame(id = "064",
                          labelSpanish = "UPCT",
                          labelEnglish = "UPCT",
@@ -44,12 +50,21 @@ nodes_tipo <-
          fullNameSpanish = labelSpanish,
          fullNameEnglish = labelEnglish) %>%
                                  distinct
-
+## -----------------------------------------------------------------------------
+## Para los nodos de planes, cargamos los acrónimos desde un excel
+## labels.wb <-
+##   openxlsx::loadWorkbook(datafile("labels.xlsx") )
+labels <- read.csv(datafile("labels.csv"),
+                   header = TRUE, colClasses = "character")
+## We check that all programmes of `alumnos` are in `planes`.
+if (sum(!alumnos$PLAN_ID %in% labels$PLAN_ID) >0){
+  stop("Alguna titulacion de alumnos no está en planes")
+}
+alumnos <- alumnos %>%
+  left_join(labels)
 nodes_planes <- with(alumnos,
        data.frame(id = nivel4,
-                  labelSpanish = ifelse(PLAN_ACRONIMO == "",
-                                        PLAN_DESC,
-                                        PLAN_ACRONIMO),
+                  labelSpanish = labelSpanish,
                   fullNameSpanish = PLAN_DESC)) %>%
   mutate(color = "#009933",
          labelEnglish = labelSpanish,
@@ -61,4 +76,51 @@ nodes_info <-
         nodes_tipo,
         nodes_planes)
 write.csv(nodes_info, "../data/nodes_info.csv",
+          row.names = FALSE)
+## -----------------------------------------------------------------------------
+##
+## To create the links
+##
+## -----------------------------------------------------------------------------
+links_12<- alumnos %>%
+  select(nivel1, nivel2) %>%
+  distinct
+names(links_12) <- c("source", "target")
+links_23 <- alumnos %>%
+  select(nivel2, nivel3) %>%
+  distinct
+names(links_23) <- c("source", "target")
+links_34 <- alumnos %>%
+  select(nivel3, nivel4) %>%
+  distinct
+names(links_34) <- c("source", "target")
+links <- rbind(links_12, links_23, links_34)
+write.csv(links, "../data/links2.csv",
+          row.names = FALSE)
+## -----------------------------------------------------------------------------
+##
+## To compute the number of students, gender distribution, etc..
+##
+## -----------------------------------------------------------------------------
+indicators <- function(alumnos, groupvariable){
+  # Convert character vector to list of symbols
+  dots <- lapply(groupvariable, as.symbol)
+  indicadores <- alumnos %>%
+    group_by_(.dots = dots) %>%
+    summarise(students = n(),
+              maleproportion = round(sum(Sexo == "Hombre") / students * 100)) %>%
+    ungroup
+  names(indicadores)[1] <- "node_id"
+  indicadores %>%
+    tidyr::gather(key = "indicator",
+                  value = "value",
+                  - node_id)
+}
+nodes_figures <-
+  rbind(indicators(alumnos, "nivel1"),
+        indicators(alumnos, "nivel2"),
+        indicators(alumnos, "nivel3"),
+        indicators(alumnos, "nivel4")) %>%
+  arrange(node_id, desc(indicator))
+write.csv(nodes_figures, file = "../data/nodes_figures.csv",
           row.names = FALSE)
